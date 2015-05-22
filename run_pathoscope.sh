94eefdc4269d7455c10ff54e759bf1bc95094549
Johnson Lab <jlab@bayes.bumc.bu.edu>
2015-05-21 21:13:20 -0400

    initial working loop

--- run_pathoscope.sh
+++ run_pathoscope.sh
@@ -6,14 +6,47 @@ pathoscope=/Users/evan/apps/pathoscope2/pathoscope/./pathoscope.py
 #Go to home directory:
 cd /Users/jlab/nick/patho_practice
 
+#Value Assignment
+LIB_DIR='~evan/apps/genome_library'
+TARGET='virus_nt_ti'
+#FILTER=
+
 ### Download FASTQ, unzip, make smaller version (remove later)
 # Now just download a single fastq file (change this later to loop over
 # all files)
 
-cp /Volumes/My\ Book/RNA\ Data/20120920-W18987.FASTQs/941017B-31_L7.LB27_2.fastq.gz 941017B-31_L7.LB27_2.fastq.gz #Nick note the backslashes in My\ Book and RNA\ DATA. these are needed in a directory name anytime you have spaces
+# cannot assign: FQ_DIR = "./*/*.fastq.gz"
+cd /Volumes/My\ Book/RNA\ Data/20120920-W18987.FASTQs/
+
+
+for FILE in ./*.fastq.gz
+
+do
+	echo 'Your file path is '$FILE
+	NEW_FILE=$(basename $FILE)
+	echo 'Your file is '$NEW_FILE
+	echo 'copying' $NEW_FILE
+	cp $NEW_FILE /Users/jlab/nick/patho_practice
+	cd /Users/jlab/nick/patho_practice
+	echo 'unzipping'
+	gunzip $NEW_FILE
+#Alignment
+	echo 'beginning pathoMap'
+LIB_DIR='~evan/apps/genome_library'
+TARGET='virus_nt_ti'
+#FILTER=	
+
+
+	cp $FILE /Users/jlab/nick/patho_practice
+	cd /Users/jlab/nick/patho_practice
+	gunzip $FILE
+	# header file for practice: head -2000 $FILE > $FILE_practice.fastq
+	
+
+# cp /Volumes/My\ Book/RNA\ Data/20120920-W18987.FASTQs/941017B-31_L7.LB27_2.fastq.gz 941017B-31_L7.LB27_2.fastq.gz #Nick note the backslashes in My\ Book and RNA\ DATA. these are needed in a directory name anytime you have spaces
 # copies to correct folder under me
-gunzip -f 941017B-31_L7.LB27_2.fastq.gz # note: I added -f so it will overwrite existing destination file if it exists
-head -4000 941017B-31_L7.LB27_2.fastq > practice.fastq
+# gunzip -f 941017B-31_L7.LB27_2.fastq.gz # note: I added -f so it will overwrite existing destination file if it exists
+# head -4000 941017B-31_L7.LB27_2.fastq > practice.fastq
 
 ### Run pathoscope:
 
